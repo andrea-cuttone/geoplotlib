9a3132f664aef54c8900fe2100d70bd353558662
Andrea Cuttone <andreacuttone@gmail.com>
2015-06-22 14:50:52 +0200

    fixed delaunay example

--- examples/delaunay.py
+++ examples/delaunay.py
@@ -7,7 +7,7 @@ from geoplotlib.utils import read_csv, BoundingBox
 
 
 data = read_csv('data/bus.csv')
-geoplotlib.add_layer(DelaunayLayer(data, cmap='hot_r'))
+geoplotlib.delaunay(data, cmap='hot_r')
 geoplotlib.set_bbox(BoundingBox.DK)
 geoplotlib.set_smoothing(True)
 geoplotlib.show()
