3b75c7d371f854ed3e42913da4d5c030f20afed0
Andrea Cuttone <andreacuttone@gmail.com>
2016-06-21 15:36:29 +0200

    version 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='geoplotlib',
       use_2to3=True,
       packages=['geoplotlib'],
-      version='0.3.0',
+      version='0.3.1',
       description='python toolbox for geographic visualizations',
       author='Andrea Cuttone',
       author_email='andreacuttone@gmail.com',
