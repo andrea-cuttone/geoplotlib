97ae83fc05d19237db79be66eb577906c35e8db5
RyanAugust <rad19206@yahoo.com>
2018-02-28 08:46:05 -0800

    adding requierments

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(name='geoplotlib',
       author='Andrea Cuttone',
       author_email='andreacuttone@gmail.com',
       url='https://github.com/andrea-cuttone/geoplotlib',
+      install_requires=['numpy>=1.12','pyglet>=1.2.4']   
 )
