45bd7778ace745ad147667bef1627e67684f8339
asdasfasasfas <103545657+asdasfasasfas@users.noreply.github.com>
2022-06-13 22:52:25 +0900

    Update geoplotlib_heatmap.py

--- workingdir/geoplotlib_heatmap.py
+++ workingdir/geoplotlib_heatmap.py
@@ -3,7 +3,7 @@ from geoplotlib.utils import read_csv, BoundingBox, DataAccessObject
 import pandas as pd
 
 df = pd.read_csv('무장애여행정보.csv')
-df.columns = ['State', 'District', '위도', '경도']
+df.columns = ['District', 'State', '경도', '위도']
 geoplotlib.kde(df, bw=8, cut_below=1e+4)
 geoplotlib.show()
 geoplotlib.set_bbox(BoundingBox.jeju)
